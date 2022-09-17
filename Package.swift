29b3a603dc68b560e00906cd84dd19040bb6a8c9
Martin Pfundmair <dev@martin.pfundmair.de>
2022-09-17 20:12:55 +0900

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MyApp",
+    dependencies: [
+        .package(url: "https://github.com/martinp7r/swift-argument-parser",
+                 branch: "testhelper_product"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "MyApp",
+            dependencies: []),
+        .testTarget(
+            name: "MyAppTests",
+            dependencies: [
+                "MyApp",
+                .product(name: "ArgumentParserTestHelpers",
+                         package: "swift-argument-parser")
+            ]),
+    ]
+)
